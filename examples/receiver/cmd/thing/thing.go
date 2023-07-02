1b13e81bf9909225c72405476e2b10b07d203b55
Bradley Garrod <bradleybredgarrod@gmail.com>
2023-07-02 16:45:00 +0100

    test: Update receiver test to use unused function in main

--- examples/receiver/cmd/thing/thing.go
+++ examples/receiver/cmd/thing/thing.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"test-only-example-receiver/pkg/other"
 	"test-only-example-receiver/pkg/stuff"
 )
 
 func main() {
+	usedFuncMain()
 	s := stuff.NewStuffStruct()
 	s.UsedFunction()
 	s.UsedPointerFunction()
 	other.OtherUsedFunc()
 	s.UsedFunction()
+}
+
+func usedFuncMain() {
+	fmt.Println("i am used")
+}
+
+func notUsedFuncMain() {
+	fmt.Println("im not used")
 }
