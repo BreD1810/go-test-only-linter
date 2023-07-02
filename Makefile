d7cc54906d4c514020246b09bc57da14ed51b8d8
Bradley Garrod <bradleybredgarrod@gmail.com>
2023-07-02 13:36:40 +0100

    feat: Print all functions in provided files

--- Makefile
+++ Makefile
@@ -4,4 +4,7 @@ build:
 run:
 	@go run cmd/go-test-only-linter/go-test-only-linter.go
 
+install:
+	go install cmd/go-test-only-linter/go-test-only-linter.go
+
 .PHONY: build run
--- cmd/go-test-only-linter/go-test-only-linter.go
+++ cmd/go-test-only-linter/go-test-only-linter.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+	"os"
+)
+
+type visitor struct {
+	fset *token.FileSet
+}
 
 func main() {
-	fmt.Println("test")
+	v := visitor{fset: token.NewFileSet()}
+	for _, filePath := range os.Args[1:] {
+		if filePath == "--" { // Needed for `go run ... -- file`
+			continue
+		}
+
+		fmt.Printf("PROVIDED ARG: %s\n", filePath)
+
+		f, err := parser.ParseFile(v.fset, filePath, nil, 0)
+		if err != nil {
+			log.Fatalf("failed to parse file %s: %s", filePath, err)
+		}
+
+		ast.Walk(&v, f)
+	}
+}
+
+func (v *visitor) Visit(node ast.Node) ast.Visitor {
+	if node == nil {
+		return nil
+	}
+
+	if fdecl, ok := node.(*ast.FuncDecl); ok {
+		fmt.Printf("Found function declaration: %s\n", fdecl.Name.Name)
+	}
+
+	return v
 }
