f31672a9b509276044ae932ebde3f711c848da81
Bradley Garrod <bradleybredgarrod@gmail.com>
2023-07-02 16:26:42 +0100

    test: Update receiver test

--- examples/receiver/pkg/stuff/stuff_test.go
+++ examples/receiver/pkg/stuff/stuff_test.go
@@ -1,20 +1,22 @@
 package stuff_test
 
 import (
-	stuff "test-only-example-big/pkg"
+	"test-only-example-receiver/pkg/stuff"
 	"testing"
 )
 
 func TestUsedFunction(t *testing.T) {
-	res := stuff.UsedFunction()
-	if res != 1 {
-		t.Fatal("usedFunction did not return 1")
+	s := stuff.NewStuffStruct()
+	res := s.UsedFunction()
+	if res != "testStruct" {
+		t.Fatal("UsedFunction did not return testStruct")
 	}
 }
 
 func TestNotUsedFunction(t *testing.T) {
-	res := stuff.NotUsedFunction()
-	if res != 2 {
-		t.Fatal("notUsedFunction did not return 1")
+	s := stuff.NewStuffStruct()
+	res := s.NotUsedFunction()
+	if res != "testStruct" {
+		t.Fatal("NotUsedFunction did not return testStruct")
 	}
 }
